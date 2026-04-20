35c113d482a4dc34d3e58f96ff95d3f7638fb4ff
Riya Torgal <riyatorgal@gmail.com>
2026-04-20 11:25:19 +0530

    fix: comment out PastAchievementsSection import and usage in HomePage

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ import {ContactSection} from "./components/ContactSection"
 import HeroSectionWrapper from "./components/HeroSectionWrapper"
 import AboutSectionWrapper from "./components/AboutSectionWrapper"
 import ExpertiseWrapper from "./components/ExpertiseWrapper"
-import {PastAchievementsSection} from "./components/PastAchievementsSection"
+// import {PastAchievementsSection} from "./components/PastAchievementsSection"
 // import { EuropeanAdvisory } from "./components/EuropeanAdvisory"
 // import { CoreFocusSection } from "./components/CoreFocus"
 
@@ -26,7 +26,7 @@ export default function HomePage() {
       <AboutSectionWrapper />
       <ExpertiseWrapper />
       <TrainingsSection />
-      <PastAchievementsSection />
+      {/* <PastAchievementsSection /> */}
       <ContactSection />
       {/* <EventsSection /> */}
       {/* <GallerySection /> */}
